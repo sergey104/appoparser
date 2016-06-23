8fb4da3bc584d43f64da8810631ec11f8b7364f1
fil <fil@localhost.localdomain>
2016-06-23 18:42:17 +0300

    new

--- main.cpp
+++ main.cpp
@@ -8,18 +8,26 @@ int main() {
     ifstream ifs("/home/fil/appodeal.txt");
     std::string ss;
     
-    std::getline(ifs, ss);
-    cout << ss;
+    while( std::getline(ifs, ss)) {
+      if(ss.empty()) continue;
+      if(ss == "-------") continue;
       
     Json::Reader reader;
     Json::Value obj;
     reader.parse(ss, obj); // reader can also read strings
     cout << "id: " << obj["id"].asString() << endl;
+    const Json::Value& app = obj["app"]; // array of characters
+    cout << "new id: " << app["id"].asString() << endl;
+    cout << "name: " << app["name"].asString() << endl;
+
     /*    cout << "Year: " << obj["year"].asUInt() << endl;
     const Json::Value& characters = obj["characters"]; // array of characters
     for (int i = 0; i < characters.size(); i++){
         cout << "    name: " << characters[i]["name"].asString();
         cout << " chapter: " << characters[i]["chapter"].asUInt();
         cout << endl;
+
 	} */
+    }
 }
+    
